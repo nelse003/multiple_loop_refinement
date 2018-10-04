9e7cc8a35b68ffa57256046f5ecd1588d349aa1a
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-10-04 09:59:47 +0100

    Refienement of multiple loop structures file started. copying of mtz files to use for refinement.

---
+++ refine_states.py
@@ -0,0 +1,40 @@
+import os
+from shutil import copyfile
+
+ref_path = "/dls/labxchem/data/2017/lb18145-17/processing/reference/"
+refinement_folder = "/dls/labxchem/data/2017/lb18145-17/processing/analysis/multiple_loop_refinements"
+data_folder =  "/dls/labxchem/data/2017/lb18145-17/processing/analysis/initial_model_rearrangement"
+
+base_pdb = os.path.join(ref_path, "XX02KALRNA-x0074-ground-state.pdb")
+rearranged_pdb = os.path.join(ref_path,"dimple_rearrange_correct_residues.pdb")
+multiple_loop_pdb = os.path.join(ref_path,"multiple_loop.pdb")
+
+input_pdbs = {base_pdb:"base",
+              rearranged_pdb:"rearranged",
+              multiple_loop_pdb: "multiple"}
+
+if not os.path.exists(refinement_folder):
+    os.mkdir(folder)
+
+for dataset_folder in data_folder:
+
+    dataset_copy_folder = os.path.join(refinement_folder,
+                                       dataset_folder)
+
+    if os.path.exists(dataset_copy_folder):
+        os.mkdir(dataset_copy_folder)
+
+    dataset = os.path.basename(dataset_copy_folder)
+    free_mtz = os.path.join(dataset_folder, "{}.free.mtz".format(dataset))
+
+    free_mtz_dst = os.path.join(dataset_copy_folder, "{}.free.mtz".format(dataset))
+
+    if not os.path.exists(free_mtz_dst):
+        copyfile(free_mtz, free_mtz_dst)
+
+
+
+
+
+
+
